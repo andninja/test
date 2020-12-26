eb025386922b57750a5777412f570bcddbd9e283
Ankaj <goyal.ankaj@tftus.com>
2020-12-26 11:36:58 +0530

    fixed imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
 import App from './App';
@@ -12,7 +12,7 @@ import './assets/scss/style.scss';
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={history}>
+  <Router history={history} basename={window.location.pathname || ''}>
     <App />
   </Router>,
   document.getElementById('root')
