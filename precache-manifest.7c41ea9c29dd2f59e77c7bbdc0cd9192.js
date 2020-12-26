c7a05344e3960897e539730cad01565e036bff6c
Ankaj <goyal.ankaj@tftus.com>
2020-12-26 11:09:52 +0530

    first commit

---
+++ precache-manifest.7c41ea9c29dd2f59e77c7bbdc0cd9192.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3cc5f4ad01db1b14885d3c7b13d18c",
+    "url": "/jashneghar/index.html"
+  },
+  {
+    "revision": "ccec0e5dc3af50d56b36",
+    "url": "/jashneghar/static/css/main.12babb87.chunk.css"
+  },
+  {
+    "revision": "cb170ed7f435afc85587",
+    "url": "/jashneghar/static/js/2.27505fc1.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/jashneghar/static/js/2.27505fc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccec0e5dc3af50d56b36",
+    "url": "/jashneghar/static/js/main.ac318506.chunk.js"
+  },
+  {
+    "revision": "d148aed1e4d19cb91bc0",
+    "url": "/jashneghar/static/js/runtime-main.c55ad6af.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/jashneghar/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/jashneghar/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/jashneghar/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/jashneghar/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/jashneghar/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/jashneghar/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/jashneghar/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/jashneghar/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/jashneghar/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "6880387d8901da36352fea41c27d7046",
+    "url": "/jashneghar/static/media/image3.6880387d.jpeg"
+  },
+  {
+    "revision": "ed19f3646b2bfdd9cc55ccf77fbc6c0b",
+    "url": "/jashneghar/static/media/image4.ed19f364.jpeg"
+  },
+  {
+    "revision": "9bda63aee5b3a0634a94fae031a0e853",
+    "url": "/jashneghar/static/media/image5.9bda63ae.jpeg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/jashneghar/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/jashneghar/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
---
+++ src/components/sections/FeaturesSplit.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionSplitProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+import Image from '../elements/Image';
+
+const propTypes = {
+  ...SectionSplitProps.types
+}
+
+const defaultProps = {
+  ...SectionSplitProps.defaults
+}
+
+const FeaturesSplit = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  invertMobile,
+  invertDesktop,
+  alignTop,
+  imageFill,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'features-split section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'features-split-inner section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const splitClasses = classNames(
+    'split-wrap',
+    invertMobile && 'invert-mobile',
+    invertDesktop && 'invert-desktop',
+    alignTop && 'align-top'
+  );
+
+  const sectionHeader = {
+    title: 'What we do',
+    paragraph: 'Does chilling with a group at nice place interest you then join us in this journey of knowing the unknown '
+  };
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={splitClasses}>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+                {/* <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Lightning fast workflow
+                  </div> */}
+                <h3 className="mt-0 mb-12">
+                  Fun Overloaded
+                  </h3>
+                <p className="m-0">
+                  If you want to have the fun of your life, dont just sit idle and join us for the lifetime experience.
+                  Do the things which you always wanted to with jashn-e-ghar
+                  </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  // src={require('./../../assets/images/features-split-image-01.png')}
+                  src={require('./../../assets/images/image3.jpeg')}
+
+                  alt="Features split 01"
+                  width={528}
+                  height={396} />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
+                {/* <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Lightning fast workflow
+                  </div> */}
+                <h3 className="mt-0 mb-12">
+                Data-driven insights
+                  </h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  // src={require('./../../assets/images/features-split-image-02.png')}
+                  src={require('./../../assets/images/image4.jpeg')}
+                  alt="Features split 02"
+                  width={528}
+                  height={396} />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+                {/* <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Lightning fast workflow
+                  </div> */}
+                <h3 className="mt-0 mb-12">
+                Data-driven insights
+
+                  </h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  // src={require('./../../assets/images/features-split-image-03.png')}
+                  src={require('./../../assets/images/image5.jpeg')}
+                  alt="Features split 03"
+                  width={528}
+                  height={396} />
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+FeaturesSplit.propTypes = propTypes;
+FeaturesSplit.defaultProps = defaultProps;
+
+export default FeaturesSplit;
